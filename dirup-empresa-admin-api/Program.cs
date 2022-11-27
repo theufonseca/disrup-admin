e28fa723bd782efa42cedadb86f28b46906312ac
João Matheus <joaomatheus_fonseca@hotmail.com>
2022-11-26 22:41:51 -0300

    ajuste na configuração de cors

--- dirup-empresa-admin-api/Program.cs
+++ dirup-empresa-admin-api/Program.cs
@@ -7,7 +7,21 @@ using Infra.Storage.Services;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.AllowAnyOrigin();
+                          policy.AllowAnyMethod();
+                          policy.AllowAnyHeader();
+                      });
+});
+
 
 // Add services to the container.
 builder.Services.AddMediatR(AppDomain.CurrentDomain.GetAssemblies());
@@ -38,6 +52,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseExceptionHandler("/error");
 
